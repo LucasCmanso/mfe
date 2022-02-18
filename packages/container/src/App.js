efa73165059246bd0d49eb331c01b3808f495d09
Lucas <lucas.cmanso@outlook.com>
2022-02-17 21:31:02 -0300

    Generate classnames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
+import { createGenerateClassName, StylesProvider } from '@material-ui/core';
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co'
+});
 
 export default () => {
     return (
-        <>
-        <h1>Sucess!!!!</h1>
-        <MarketingApp />
-        </>
+        <StylesProvider generateClassName={generateClassName}>
+            <BrowserRouter>
+                <Header />
+                <MarketingApp />
+            </BrowserRouter>
+        </ StylesProvider>
+
     )
 }
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import {Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import { createGenerateClassName, StylesProvider } from '@material-ui/core/styles';
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'ma'
+});
 
 export default () => {
     return (
-    <StylesProvider>
+    <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
             <switch>
                 <Route exact path="/" component={Landing}></Route>
