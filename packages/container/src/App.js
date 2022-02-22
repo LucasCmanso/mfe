6d389e586be9ca7924f401ec3c3a320e650e2c6d
Lucas <lucas.cmanso@outlook.com>
2022-02-22 15:35:08 -0300

    Fix marketing

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -23,9 +23,7 @@ export default () => {
                         <Route path="/auth">
                             <AuthLazy onSignIn={() => setIsSignedIn(true)} />
                         </Route>
-                        <Route path="/">
-                            <MarketingLazy />
-                        </Route>
+                        <Route path="/" component={MarketingLazy} />
                     </Switch>
                 </Suspense>
             </BrowserRouter>
